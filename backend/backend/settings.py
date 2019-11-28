d597f95c3673c762c8e157e12e5201ca283f7d9a
dbpython <dbpython>
2019-11-28 21:55:03 +0000

    added rest_framework apps to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,13 +31,20 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+
+    # Local
+    'api',
+
+    # Third-party
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
