726a9b3d5c4e6ba3c05d85cbcfc3f88be53ac458
dbpython <dbpython>
2019-11-28 22:21:21 +0000

    added Rest framework auhentication settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,6 +47,15 @@ INSTALLED_APPS = [
     'rest_framework.authtoken'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
